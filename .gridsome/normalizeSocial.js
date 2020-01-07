06b6973e44a57f41c75fb1661aaba06d09b4c2dc
Xananax <xananax@yelostudio.com>
2020-01-07 15:51:02 +0300

    added social icons

---
+++ .gridsome/normalizeSocial.js
@@ -0,0 +1,49 @@
+const parseItem = provider => {
+  if (!provider) {
+    return false;
+  }
+  const raw =
+    typeof provider === "string"
+      ? provider
+      : provider.url
+      ? provider.url
+      : null;
+  if (!raw) {
+    return false;
+  }
+  const [, raw_protocol, address] = raw.match(
+    /(mailto|tel|bitcoin|(?:(?:ht|f)tps?|\/\/)):?(?:\/\/)?\s*?(.*)/i
+  );
+  if (!address) {
+    return false;
+  }
+  const protocol =
+    raw_protocol === "//" || raw_protocol === "/" || raw_protocol === ""
+      ? "https"
+      : raw_protocol;
+  const is_web = /(ht|f)tps?/.test(protocol);
+  const name = provider.name
+    ? provider.name
+    : is_web
+    ? address
+        .replace(/\/.*?$/, "")
+        .trim()
+        .split(".")
+        .reverse()[1]
+    : protocol === "mailto"
+    ? "mail"
+    : protocol === "tel"
+    ? "number"
+    : protocol;
+  const icon = provider.icon
+    ? provider.icon.split(/\s+/g)
+    : ["fab", name.toLowerCase()];
+  const url = protocol + (is_web ? "://" : ":") + address;
+  return { name, protocol, address, icon, url };
+};
+
+const parseSocialArray = socialArray =>
+  socialArray.map(parseItem).filter(Boolean);
+parseSocialArray.parseItem = parseItem;
+
+module.exports = parseSocialArray;
--- src/config/fontAwesome.js
+++ src/config/fontAwesome.js
@@ -1,24 +1,32 @@
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 import { config, library } from "@fortawesome/fontawesome-svg-core";
-// To know what icons are available, look into 
+// To know what icons are available, look into
 // https://fontawesome.com/icons?d=gallery&s=brands&m=free
 // ADD BRANDS HERE:
 import {
-  faGithub,
+  faMastodon,
   faGitlab,
+  faReddit,
   faTwitter,
-  faMastodon
+  faGithub,
+  faTelegram,
+  faSkype,
+  faCodepen,
+  faKeybase,
+  faLinkedin
 } from "@fortawesome/free-brands-svg-icons";
-// To know what icons are available, look into 
+// To know what icons are available, look into
 // https://fontawesome.com/icons?d=gallery&s=solid&m=free
 // ADD ICONS HERE:
-import { 
-  faSpinner, 
-  faChessQueen,
+import {
+  faSpinner,
   faHome,
   faNewspaper,
   faRss,
-  faAtom
+  faAtom,
+  faCommentDots,
+  faCode,
+  faEnvelope
 } from "@fortawesome/free-solid-svg-icons";
 import "@fortawesome/fontawesome-svg-core/styles.css";
 
@@ -27,16 +35,25 @@ config.autoAddCss = false;
 // UPDATE THIS LIST TO SYNC WITH THE ICONS
 // YOU'VE SELECTED ABOVE
 library.add(
-  faGithub,
-  faGitlab,
-  faTwitter,
   faMastodon,
+  faGitlab,
+  faReddit,
+  faTwitter,
+  faGithub,
+  faTelegram,
+  faSkype,
+  faCodepen,
+  faKeybase,
+  faLinkedin,
+  //
   faSpinner,
-  faChessQueen,
   faHome,
   faNewspaper,
   faRss,
-  faAtom
+  faAtom,
+  faCommentDots,
+  faCode,
+  faEnvelope
 );
 
 export default FontAwesomeIcon;
